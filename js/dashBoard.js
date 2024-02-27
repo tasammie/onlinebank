f07a42fc601e4d24dc96b8352d2aea1188b1c80c
tasammie <temiloluwasamuel4@gmail.com>
2024-02-27 11:34:31 +0100

    new update

--- js/dashBoard.js
+++ js/dashBoard.js
@@ -1,54 +1,23 @@
-    import{db, auth } from './index.js';
-    import {
-      getDoc,
-      collection,
-      doc,
-      getDocs,
-      query,
-      where
-    } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-    // const colRef = collection(db, "Account");
+import { db, auth } from "./index.js";
+import {
+  getDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-    // let ALLDetails = []
-    // async function getAllDataFromDB(){
-    //   ALLDetails = []
-    //     const res = await getDocs(colRef)
-    //     console.log(res)
-    //     res.forEach((info)=>{
-            
-    //       ALLDetails.push({...info.data(), id: info.id})
-    //     })
-    //     displayDataOnUi(ALLDetails)
+import { logOutUser } from "./index.js";
 
+const colRef = collection(db, "Account");
 
-    // }
-
-    // getAllDataFromDB()
-    // // let blogID = ""
-
-    // function displayDataOnUi(data){
-    //     show.innerHTML = ''
-    //     console.log(data);
-    //     data.forEach((info)=>{
-    //         show.innerHTML +=`
-    //             <div>
-    //                 <h1>${info.accountNumber}</h1>    
-    //                 <p>${info.ref}</p>
-    //                 <p>${info.balance}</p>
-    //             </div>
-    //         `
-    //       })
-
-    // }
-
-
-    const colRef = collection(db, "Account");
-  
 let ALLDetails = [];
 
 async function getAllDataFromDB() {
   ALLDetails = [];
   const user = auth.currentUser;
+  console.log(user);
   if (user) {
     const userId = user.uid;
     const q = query(colRef, where("userId", "==", userId));
@@ -66,7 +35,7 @@ getAllDataFromDB();
 
 function displayDataOnUi(data) {
   const show = document.getElementById("show");
-  show.innerHTML = '';
+  show.innerHTML = "";
   data.forEach((info) => {
     show.innerHTML += `
       <div>
@@ -78,32 +47,44 @@ function displayDataOnUi(data) {
   });
 }
 
+logout.addEventListener("click", logOutUser);
 
+// Side bar toggling of the sidebar
+const toggleButton = document.querySelector(".toggle-button");
+const cancelBtn = document.querySelector(".cancel-btn");
+const sidebar = document.querySelector(".sidebar");
+const sidebarBg = document.getElementById("sidebar-bg");
+const content = document.querySelector(".content");
+const toggleBtn = document.getElementById("toggleButton");
+const sidebarLinks = document.querySelectorAll(".sidebar-Link");
 
+toggleBtn.addEventListener('click', function() {
+  if(toggleBtn.firstElementChild.classList.contains('fa-bars')) {
+    toggleBtn.firstElementChild.classList.remove('fa-bars')
+    toggleBtn.firstElementChild.classList.add('fa-times')
+  } else {
+    toggleBtn.firstElementChild.classList.remove('fa-times')
+    toggleBtn.firstElementChild.classList.add('fa-bars')
+  }
+})
 
+sidebarBg.addEventListener("click", function () {
+  sidebar.classList.remove("active");
+  sidebarBg.style.display = "none";
+  toggleBtn.firstElementChild.classList.remove('fa-times')
+  toggleBtn.firstElementChild.classList.add('fa-bars')
+});
 
+toggleButton.addEventListener("click", function () {
+  sidebar.classList.toggle("active");
+  if(sidebarBg.style.display == "block") {
+    sidebarBg.style.display = "none";
+    return;
+  }
+  sidebarBg.style.display = "block";
+});
 
-
-
-
-
-
-
-    // Side bar toggling of the sidebar
-    const toggleButton = document.querySelector(".toggle-button");
-      const cancelBtn = document.querySelector(".cancel-btn");
-      const sidebar = document.querySelector(".sidebar");
-      const content = document.querySelector(".content");
-      const sidebarLinks = document.querySelectorAll(".sidebar-Link");
-
-      toggleButton.addEventListener("click", function () {
-        sidebar.classList.toggle("active");
-      });
-
-      cancelBtn.addEventListener("click", function () {
-        sidebar.classList.remove("active");
-      });
-
-      
-
-
+cancelBtn.addEventListener("click", function () {
+  sidebar.classList.remove("active");
+  sidebarBg.style.display = "none";
+});
--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ import {
   doc,
   getDocs,
   query,
-  where
+  where,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import {
   getAuth,
@@ -15,7 +15,7 @@ import {
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
-  sendPasswordResetEmail 
+  sendPasswordResetEmail,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
@@ -28,50 +28,76 @@ const firebaseConfig = {
   measurementId: "G-6844P7CG07",
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const colRef = collection(db, "users");
 export const auth = getAuth(app);
-export const signInUser = async (email, password)=>{
+export const signInUser = async (email, password) => {
   const message = {
-    success:'',
-    message:''
-  }
+    success: "",
+    message: "",
+  };
   try {
-    const res = await signInWithEmailAndPassword(auth,email,password)
-    message.success = true
-    message.message = 'Successfully signed in'
-    message.result = res
-    return message
+    const res = await signInWithEmailAndPassword(auth, email, password);
+    message.success = true;
+    message.message = "Successfully signed in";
+    message.result = res;
+    return message;
   } catch (error) {
-    message.success = false
-    message.message = 'failed to sign in'
-    message.error = error
-    return message
+    message.success = false;
+    message.message = "failed to sign in";
+    message.error = error;
+    return message;
   }
 };
-export const sendResetEmail =  async (email) => {
-  sendPasswordResetEmail(auth, email)
-}
+export const sendResetEmail = async (email) => {
+  sendPasswordResetEmail(auth, email);
+};
+export const logOutUser = async (email) => {
+  signOut(auth, email);
+  console.log("Logout successful!");
+  // localStorage.removeItem("user");
+};
 
 onAuthStateChanged(auth, async (user) => {
+  userDetails.innerHTML = "";
   if (user) {
-    const q = query(colRef, where("email", "==", user.email));
+    const q = query(colRef, where("email", "==", user?.email));
     const querySnapshot = await getDocs(q);
-
+    console.log(querySnapshot.empty)
     if (!querySnapshot.empty) {
       const docSnapshot = querySnapshot.docs[0];
       const data = docSnapshot.data();
-      console.log(data);
+      console.log(data)
       try {
-        userDetails.innerHTML = data.firstName + " " + data.lastName
+        userDetails.innerHTML = data.firstName + " " + data.lastName;
       } catch (error) {
-        return
+        return;
       }
     } else {
+      userDetails.innerHTML = "";
       console.log("No user found with the email:", user.email);
     }
   }
 });
+
+// onAuthStateChanged(auth, async (user) => {
+//   if (user) {
+//     const q = query(colRef, where("email", "==", user.email));
+//     const querySnapshot = await getDocs(q);
+
+//     if (!querySnapshot.empty) {
+//       const docSnapshot = querySnapshot.docs[0];
+//       const data = docSnapshot.data();
+//       console.log(data);
+//       try {
+//         userDetails.innerHTML = data.firstName + " " + data.lastName
+//       } catch (error) {
+//         return
+//       }
+//     } else {
+//       console.log("No user found with the email:", user.email);
+//     }
+//   }
+// });
